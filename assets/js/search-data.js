1f55fbab01c6ad98d18db91f1d193439f4ea9cb6
caterina-doglioni <caterina.doglioni@cern.ch>
2026-01-20 06:52:39 +0000

    Deploying to gh-pages from @ caterina-doglioni/caterina-doglioni.github.io@5ab7817417ff470768c9f762d6278342d9fe3b4e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,8 +45,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "Teaching experience and courses.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
