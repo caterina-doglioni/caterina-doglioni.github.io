37c0c4fa48371969b98a43dfacb6a4877fb59d5a
caterina-doglioni <caterina.doglioni@cern.ch>
2026-01-23 07:33:49 +0000

    Deploying to gh-pages from @ caterina-doglioni/caterina-doglioni.github.io@26495fe97e7d5e2598bd9f1fbf90a14f95836df3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-outreach",
           title: "outreach",
-          description: "A growing collection of outreach activities.",
+          description: "A growing collection of outreach activities, categorised by audience.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/outreach/";
